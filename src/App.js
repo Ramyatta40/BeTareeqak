ce2dc4433dff44752d7b2c5a5fd35324a7ca5f51
SharifDaqqa <120181027+SharifDaqqa@users.noreply.github.com>
2023-03-11 00:40:48 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Map from "./components/Map";
 
 const App = () => {
   return (
+    <Fragment>
+    <Navbar />
+    <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Map" element={<Map />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        </Container>
+        
+        </Fragment>
   );
 };
 
