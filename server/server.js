0c40cbb3c8b54bea6c895915a71ced489324feea
Ramyatta40 <Ramyatta40@gmail.com>
2023-03-30 17:09:06 +0300

    efgerg

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('')
+const app = express
+
+
+app.get("/api", (req, res) => {
+
+    res.json({ "user": ["name", "pass", "number"] })
+})
+
+
+app.listen(5000, () => { console.log("Server Started at port 5000"); })
